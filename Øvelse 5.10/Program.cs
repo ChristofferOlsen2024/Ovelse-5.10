d49bdea9c8b48cecd8d9503246ba7199047adb2c
Skole <Skole@DESKTOP-87FGBUN>
2024-04-18 08:27:17 +0200

    Done

--- Øvelse 5.10/Program.cs
+++ Øvelse 5.10/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string adgangskode;
+int antal = 0;
+do
+{
+    Console.WriteLine("Indtast dit password");
+    adgangskode = Console.ReadLine();
+    antal++;
+} while (adgangskode != "password" && antal < 3);
